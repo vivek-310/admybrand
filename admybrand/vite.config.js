683f7e005292f6cb66b90159d020adb597ee41da
vivek-310 <vivekvardhan0310@gmail.com>
2025-07-29 19:25:36 +0530

    deploy fix

--- admybrand/vite.config.js
+++ admybrand/vite.config.js
@@ -1,33 +1,33 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { splitVendorChunkPlugin } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
   plugins: [
-    react(),
-    splitVendorChunkPlugin()
+    react()
   ],
   build: {
     outDir: 'dist',
     rollupOptions: {
       output: {
-        manualChunks: {
-          'react-vendor': ['react', 'react-dom'],
-          'framer-motion': ['framer-motion'],
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react/') || id.includes('react-dom/')) {
+              return 'react-vendor';
+            }
+            if (id.includes('framer-motion/')) {
+              return 'framer-motion';
+            }
+            return 'vendor';
+          }
         }
       }
     },
     chunkSizeWarningLimit: 1000,
     cssCodeSplit: true,
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true
-      }
-    }
+    minify: 'esbuild',
+    target: 'esnext'
   },
   server: {
     open: true,
